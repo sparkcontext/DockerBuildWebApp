1e4d92bf8b8bd7151f112348178389eecc5a8a23
User <User@DESKTOP-OQS8RT3>
2021-12-08 21:06:20 +0800

    hallo world

---
+++ DockerBuildWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerBuildWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        private readonly IConfiguration Configuration;
+        [BindProperty] public string HomePageTitle { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        protected void OnInitialized()
+        {
+        }
+
+        public void OnGet()
+        {
+            HomePageTitle = Configuration.GetSection("HomePageTitle").Value;
+        }
+    }
+}
